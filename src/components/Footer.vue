97d591adb606d95ffc51e8fcbf709e4761e76a4a
Lucas MAttos <lucassousa.d.m251@gmail.com>
2022-06-16 19:19:52 -0300

    making the website header

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="Footer">
+        <h1>Footer</h1>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    name: "Footer"
+}
+</script>
+
+<style scoped>
+
+</style>
