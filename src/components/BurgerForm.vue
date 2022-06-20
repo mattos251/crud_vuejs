59ded8bf2245d5a6265e5215f67d0bf005272c75
Lucas MAttos <lucassousa.d.m251@gmail.com>
2022-06-20 15:09:37 -0300

    form completo

--- src/components/BurgerForm.vue
+++ src/components/BurgerForm.vue
@@ -22,7 +22,7 @@
             </div>
 
             <div class="input-container">
-                <label for="carne"> Escolha a carne do seu burger: </label>
+                <label for="carne" > Escolha a carne do seu burger: </label>
 
                 <select name="carne" id="carne" v-model="carne">
                     <option value="">Selecione o tipo de carne</option>
@@ -34,25 +34,31 @@
 
             </div>
 
-            <div class="input-container">
-                <label for="opcionais"> Selecione outras opções: </label>
+            <div id="opcionais-container" class="input-container">
+                <label id="opcionais-title" for="opcionais" placeholder=""> Selecione outras opções: </label>
 
                 <div class="checkbox-container" >
                     <input type="checkbox" name="opcionais" v-model="opcionais" value="opcionais">
                     <span> Salame</span>
+                </div>
 
-                    <input type="checkbox" name="opcionais" v-model="opcionais" value="opcionais">
+                <div class="checkbox-container" >
+                     <input type="checkbox" name="opcionais" v-model="opcionais" value="opcionais">
                     <span> Bacon</span>
-
+                </div>  
+                
+                <div class="checkbox-container">
                     <input type="checkbox" name="opcionais" v-model="opcionais" value="opcionais">
                     <span> Cheddar</span>
-
-                    <input type="checkbox" name="opcionais" v-model="opcionais" value="opcionais">
-                    <span> Cebola roxa</span>
-  
                 </div>
 
             </div>
+
+            <div class="input-container">
+                <input type="submit" class="submit-btn" value="Criar meu Burger">
+            </div>
+
+
         </form>
 
     </div>
@@ -69,6 +75,74 @@
 
 </script>
 
-<style>
+<style scoped>
+    #burger-form{
+        width: 300px;
+        margin: auto;
+    }
+
+    .input-container{
+        display: flex;
+        flex-direction: column;
+        margin-bottom: 20px;
+    }
+
+    label{
+        font-weight: bold;
+        margin-bottom: 15px;
+        color: #222;
+        padding: 5px 10px;
+        border-left: 4px solid #fcba03;
+    }
+
+    input, select{
+        padding: 5px 10px;
+        background: none;
+        border-radius: 6px;
+        width: 300px;
+    }
+
+    #opcionais-container{
+        flex-direction: row;
+        flex-wrap: wrap;
+    }
+
+    #opcionais-title{
+        width: 100%;
+    }
+    
+    .checkbox-container{
+        display: flex;
+        align-items: flex-start;
+        width: 50%;
+        margin-bottom: 20px;
+    }
+
+    .checkbox-container span, 
+    .checkbox-container input{
+        width: auto;
+        padding-left: 10px;
+        font-weight: bold;
+
+    }
+
+
+    .submit-btn{
+        background-color: #222;
+        color: #fcba03;
+        font-weight: bold;
+        padding: 10px;
+        border: 2px solid #222;
+        font-size: 16px;
+        margin: 0 auto;
+        cursor: pointer;
+        transition: .5s;
+    }
+
+    .submit-btn:hover{
+        background-color: transparent;
+        color: #222;
+        
+    }
 
 </style>
